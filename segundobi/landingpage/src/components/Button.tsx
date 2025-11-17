082b108de80a8c273f35507d3d1470682920e2a1
Matheus <matheus22lima2005@gmail.com>
2025-11-17 19:51:51 -0300

    adicionando detalhes finais e envio por e-mail

--- segundobi/landingpage/src/components/Button.tsx
+++ segundobi/landingpage/src/components/Button.tsx
@@ -1,13 +1,15 @@
 import "../styles/buttons.css"
+import type { MouseEventHandler } from "react";
 
 interface IButtonProps {
     text: string;
     secondary?: boolean;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ text, secondary }: IButtonProps) {
+export default function Button({ text, secondary, onClick }: IButtonProps) {
     return (
-        <button className={secondary ? "btn-secondary" : "btn-primary"}>
+        <button onClick={onClick} className={secondary ? "btn-secondary" : "btn-primary"}>
             {text}
         </button>
     )
